1f6301b7f8b29f1d2d864f748de4126b4e84d1ab
Talyslan <talyslancpc@gmail.com>
2025-08-27 01:26:45 -0300

    feat (frontend): erro personalizado e exibido na tela de erro

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,13 +1,26 @@
 'use client';
 
-import { ErrorSVG, Link } from '@/components';
+import { Button, ErrorSVG, Link } from '@/components';
 
-export default function Error() {
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
     return (
         <main className="flex flex-col h-screen w-full items-center justify-center">
             <h1>Aconteceu um erro inesperado.</h1>
+
             <ErrorSVG />
-            <Link href={'/'}>Voltar ao início</Link>
+
+            <p>Erro: {error.message}</p>
+
+            <div className="flex flex-col gap-2 mt-10">
+                <Button onClick={() => reset()}>Tente novamente</Button>
+                <Link href={'/'}>Voltar ao início</Link>
+            </div>
         </main>
     );
 }
