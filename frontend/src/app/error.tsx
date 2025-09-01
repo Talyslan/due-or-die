734d143cd850c293d59ae3e3ebc3f04dfa03bc99
Talyslan <talyslancpc@gmail.com>
2025-09-01 14:34:40 -0300

    feat (frontend): adicionando refresh ao tentar novamente

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button, ErrorSVG, CustomLink } from '@/components';
+import { useRouter } from 'next/navigation';
 
 export default function Error({
     error,
@@ -9,16 +10,23 @@ export default function Error({
     error: Error & { digest?: string };
     reset: () => void;
 }) {
+    const router = useRouter();
+
+    const onTryAgain = () => {
+        reset();
+        router.refresh();
+    };
+
     return (
         <main className="flex flex-col h-screen w-full items-center justify-center">
             <h1>Aconteceu um erro inesperado.</h1>
 
             <ErrorSVG />
 
-            <p className='max-w-1/2'>Erro: {error.message}</p>
+            <p className="max-w-1/2">Erro: {error.message}</p>
 
             <div className="flex flex-col gap-2 mt-10">
-                <Button onClick={() => reset()}>Tente novamente</Button>
+                <Button onClick={() => onTryAgain()}>Tente novamente</Button>
                 <CustomLink href={'/'}>Voltar ao início</CustomLink>
             </div>
         </main>
